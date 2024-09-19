ffb69168907ab7df7cbfeb49cac22d7a34f53ddc
kanakanho <kahoshibakichi@gmail.com>
2024-09-20 00:41:01 +0900

    fix: ymlのstringへのパースをライブラリに任せる

--- src/webhook/CreateContent.ts
+++ src/webhook/CreateContent.ts
@@ -1,28 +1,12 @@
+import { stringify } from "yaml";
 import type { Frontmatter } from "../types/Frontmatter";
 
 function CreateContent(frontmatter: Frontmatter, body_md: string): string {
   // frontmatter を作成
-  const frontmatterStr = `---
-isActive: ${frontmatter.isActive}
-number: ${frontmatter.number}
-title: ${frontmatter.title}
-date: ${frontmatter.date}
-tags: ${frontmatter.tags}
-options:
-  description: ${frontmatter.options?.description}
-  repository: ${frontmatter.options?.repository}
-  youtube: ${frontmatter.options?.youtube}
-  website: ${frontmatter.options?.website}
-  image: ${frontmatter.options?.image}
-works:
-  worksDisplay: ${frontmatter.works?.worksDisplay}
-  title: ${frontmatter.works?.worksTitle}
-  description: ${frontmatter.works?.worksDescription}
-  image: ${frontmatter.works?.worksImage}
----
-`;
+  const ymlStr = stringify(frontmatter)
 
-  return frontmatterStr + body_md;
+  // frontmatter と body_md を結合
+  return `---\n${ymlStr}\n---\n${body_md}`;
 }
 
 export default CreateContent;
